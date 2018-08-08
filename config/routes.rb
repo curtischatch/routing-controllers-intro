58ca1639f11aec40ad5a51c1ab5bfe187792c3ce
curtischatch <curtis.chat.hk@gmail.com>
2018-08-08 18:05:59 -0400

    welcome path

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get '/' => 'pages#welcome'
+  root 'pages#welcome'
   get '/welcome' => 'pages#welcome'
   get '/contest' => 'pages#contest'
-  get '/kitten/:size' => 'pages#kitten'
-  get '/kittens/:size' => 'pages#kittens'
-  get '/secrets/:magic_word' => 'pages#secrets'
+  get '/kitten/:size' => 'pages#kitten', as: 'kitten'
+  get '/kittens/:size' => 'pages#kittens', as: 'kittens'
+  get '/secrets/:magic_word' => 'pages#secrets', as: 'secret'
 
 
 
