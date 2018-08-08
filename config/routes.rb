7035e48144bd52b40d103f7ab54be66c621b9a42
curtischatch <curtis.chat.hk@gmail.com>
2018-08-08 16:25:30 -0400

    kitten page route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   get '/' => 'pages#welcome'
   get '/welcome' => 'pages#welcome'
   get '/contest' => 'pages#contest'
+  get '/kitten/:size' => 'pages#kitten'
+
+
+
+
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
